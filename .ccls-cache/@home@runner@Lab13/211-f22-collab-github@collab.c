44ebc586111f834c2baca209b73cd88be5326ffb
Pedro Pardo <93295221+pedropardo14@users.noreply.github.com>
2022-11-15 16:24:13 +0000

    new printf into the collab.c on git

--- .ccls-cache/@home@runner@Lab13/211-f22-collab-github@collab.c
+++ .ccls-cache/@home@runner@Lab13/211-f22-collab-github@collab.c
@@ -23,7 +23,44 @@ void reed_ekidan2() {
 
 void ppardo3_jbolan6() {
 	printf("Pedro Pardo and Jose Bolan\n");
-	printf("");
+	printf("// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void ppardo3_jbolan6() {
+	printf("Pedro Pardo and Jose Bolan\n");
+	printf("We learned how to clone and add things to gitHub using git Commands");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+		ppardo3_jbolan6();
+    
+    return 0;
+}
+");
 }
 
 int main()
